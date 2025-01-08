f7444aeb08d9ce5aa18d8ab61bcd3c07ece8c7ce
JessicaLuana1377 <jessica.luana1377@gmail.com>
2025-01-08 16:34:29 -0300

    ultimo, ultimo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1077499213"
+    serviceWorkerVersion: "1350390149"
   }
 });
